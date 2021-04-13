19c080b2b38b46adbcef1c3e79dd1f32648f02ee
Igo go <igogo@ddd.com>
2021-04-13 16:57:42 +0300

    fix ingress and add prod certs

--- secret.sh
+++ secret.sh
@@ -1,1 +1,2 @@
-kubectl create secret tls ingress-nginx-secret-tls --cert=./argocd.polo.zeppel.in.ua/cert1.pem --key=./argocd.polo.zeppel.in.ua/privkey1.pem -n argocd
+kubectl delete secret  ingress-nginx-secret-tls -n argocd
+kubectl create secret tls ingress-nginx-secret-tls --cert=./argocd.polo.zeppel.in.ua/fullchain1.pem --key=./argocd.polo.zeppel.in.ua/privkey1.pem -n argocd
